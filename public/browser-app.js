49be4a805b569d96dfafd7d11b2471fbf5f3878c
sofian-elgharbaoui <sofiangharbaoui54@gmail.com>
2023-01-27 15:18:53 +0100

    display the tasks in the page

--- public/browser-app.js
+++ public/browser-app.js
@@ -1,16 +1,14 @@
-const tasksDOM = document.querySelector(".tasks");
-const loadingDOM = document.querySelector(".loading-text");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
 const formAlertDOM = document.querySelector(".form-alert");
+const loadingDOM = document.querySelector(".loading-text");
+const tasksDOM = document.querySelector(".tasks");
 
-// Load tasks from /api/tasks
+// Load tasks from /api/v1/tasks
 const showTasks = async () => {
   loadingDOM.style.visibility = "visible";
   try {
-    const {
-      data: { tasks },
-    } = await axios.get("/api/v1/tasks");
+    const { data: tasks } = await axios.get("/api/v1/tasks");
     if (tasks.length < 1) {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>';
       loadingDOM.style.visibility = "hidden";
@@ -19,22 +17,22 @@ const showTasks = async () => {
     const allTasks = tasks
       .map((task) => {
         const { completed, _id: taskID, name } = task;
-        return `<div class="single-task ${completed && "task-completed"}">
-<h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
-<div class="task-links">
+        console.log(taskID);
+        return `
+          <div class="single-task ${completed && "task-completed"}">
+            <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
+            <div class="task-links">
+              <!-- edit link -->
+              <a href="task.html?id=${taskID}" class="edit-link">
+                <i class="fas fa-edit"></i>
+              </a>
 
-
-
-<!-- edit link -->
-<a href="task.html?id=${taskID}"  class="edit-link">
-<i class="fas fa-edit"></i>
-</a>
-<!-- delete btn -->
-<button type="button" class="delete-btn" data-id="${taskID}">
-<i class="fas fa-trash"></i>
-</button>
-</div>
-</div>`;
+              <!-- delete btn -->
+              <button type="button" class="delete-btn" data-id="${taskID}">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          </div>`;
       })
       .join("");
     tasksDOM.innerHTML = allTasks;
@@ -44,11 +42,9 @@ const showTasks = async () => {
   }
   loadingDOM.style.visibility = "hidden";
 };
-
 showTasks();
 
-// delete task /api/tasks/:id
-
+// delete task /api/v1/tasks/:id
 tasksDOM.addEventListener("click", async (e) => {
   const el = e.target;
   if (el.parentElement.classList.contains("delete-btn")) {
@@ -56,6 +52,7 @@ tasksDOM.addEventListener("click", async (e) => {
     const id = el.parentElement.dataset.id;
     try {
       await axios.delete(`/api/v1/tasks/${id}`);
+      // reload the tasks list
       showTasks();
     } catch (error) {
       console.log(error);
@@ -65,7 +62,6 @@ tasksDOM.addEventListener("click", async (e) => {
 });
 
 // form
-
 formDOM.addEventListener("submit", async (e) => {
   e.preventDefault();
   const name = taskInputDOM.value;
