91773aa37fcfeefabb4d5f5acfaa8d1774af1362
sofian-elgharbaoui <sofiangharbaoui54@gmail.com>
2023-01-28 14:38:03 +0100

    add comments to explain the code

--- middlewares/async.js
+++ middlewares/async.js
@@ -1,8 +1,10 @@
 const asyncWrapper = (fn) => {
   return async (req, res, next) => {
     try {
+      // the fn will be used/called here
       await fn(req, res, next);
     } catch (error) {
+      // this next function will send the error to the errorHandler
       next(error);
     }
   };
